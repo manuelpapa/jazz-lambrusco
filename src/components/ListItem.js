ea3b1c78afc7a28f3557c6c84c26730c555c230b
manuelpapa <manulito@de-vio.com>
2020-08-16 14:48:20 +0200

    Add List + ListItems

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,5 +1,10 @@
 import React from "react";
+import "./ListItem.css";
 
 export default function ListItem({ children, href }) {
-  return <a href={href}>{children}</a>;
+  return (
+    <a className="listItem" href={href}>
+      {children}
+    </a>
+  );
 }
