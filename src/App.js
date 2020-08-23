86d50e5e710cc399ed37a34f0295b2808e9f5bee
manuelpapa <manulito@de-vio.com>
2020-08-23 16:55:04 +0200

    Outsource SearchInput

--- src/App.js
+++ src/App.js
@@ -8,22 +8,26 @@ import {
   Redirect,
 } from "react-router-dom";
 import Wine from "./pages/Wine";
+import GlobalStyles from "./GlobalStyles";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/wines/:lwin">
-          <Wine />
-        </Route>
-        <Route path="/wines">
-          <Wines />
-        </Route>
-        <Route path="/">
-          <Redirect to="/wines" />
-        </Route>
-      </Switch>
-    </Router>
+    <>
+      <GlobalStyles />
+      <Router>
+        <Switch>
+          <Route path="/wines/:lwin">
+            <Wine />
+          </Route>
+          <Route path="/wines">
+            <Wines />
+          </Route>
+          <Route path="/">
+            <Redirect to="/wines" />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
--- src/pages/Wines.js
+++ src/pages/Wines.js
@@ -4,6 +4,8 @@ import ListItem from "../components/ListItem";
 import List from "../components/List";
 import LoadingScreen from "../components/LoadingScreen";
 import { fetchWines } from "../api/wines";
+import styled from "@emotion/styled";
+import SearchInput from "../components/SearchInput";
 
 function Wines() {
   const [wines, setWines] = useState([]);
@@ -28,19 +30,69 @@ function Wines() {
     return wine.wine.toLowerCase().match(query.toLowerCase());
   });
 
+  const Container = styled.div`
+    height: 100vh;
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+  `;
+
+  const Header = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 20px 20px;
+    background-image: var(--bg-gradient);
+  `;
+
+  // const Input = styled("input")`
+  //   font-size: 0.9em;
+  //   padding: 10px 15px;
+  //   margin-bottom: 10px;
+  //   border: none;
+  //   border-radius: 50px;
+  //   background-color: rgba(255, 255, 255, 0.4);
+  //   color: #888888;
+  // `;
+
+  //   return (
+  //     <>
+  //       <Container>
+  //         <Header>
+  //           <small>Hello,</small>
+  //           <h2>What wine today?</h2>
+
+  //           <input
+  //             value={query}
+  //             onChange={(event) => setQuery(event.target.value)}
+  //             className="header__input"
+  //             placeholder="🔎 Search for your favorite wine"
+  //           />
+  //         </Header>
+  //         <main>
+  //           <List>
+  //             {filteredWines.map((wine) => (
+  //               <ListItem
+  //                 key={wine.lwin}
+  //                 href={`/wines/${wine.lwin_11}`}
+  //                 className="wineList"
+  //               >
+  //                 {wine.wine}
+  //               </ListItem>
+  //             ))}
+  //           </List>
+  //         </main>
+  //       </Container>
+  //     </>
+  //   );
+  // }
+
+  // VORHER
   return (
     <>
       <div className="app">
         <header>
           <small>Hello,</small>
           <h2>What wine today?</h2>
-
-          <input
-            value={query}
-            onChange={(event) => setQuery(event.target.value)}
-            className="header__input"
-            placeholder="🔎 Search for your favorite wine"
-          />
+          <SearchInput value={query} onChange={(value) => setQuery(value)} />
         </header>
         <main>
           <List>
